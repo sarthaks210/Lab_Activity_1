bf2237b9e0b65279ea9ad9ca539c927e8880941c
sarthaks210 <sarthaks210@gmail.com>
2023-02-06 16:23:29 -0500

    first model

--- Ass-2.py
+++ Ass-2.py
@@ -7,14 +7,37 @@ df = pd.read_csv("C:/Users/Sarthak/Downloads/Cancer.csv")
 
 df.head()
 
-from sklearn linear. nodel import LogisticRegression
-from sklearn. metrics import mean_squared_error
-from sklearn-nodel. selection import train_test_split
-import matelotlib-pyplot as plt
+df = df.dropna()
+
+df.shape
+
+x = df.iloc[:,:10].values
+y = df.iloc[:,10:].values
+
+x
+
+y
+
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
 
 #Split the data into 80% training and 20% testing
-*train, x_test, y_train, y_test - train test_split (x, y, test_size - 0.20)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.20)
 
 #Create the models
 #Create the 1inear regression model
-1g - LogisticRegression(). fit (%_train, y_train)
+lg = LogisticRegression().fit(x_train, y_train)
+
+#Show the model linear regression prediction
+lg_prediction = lg.predict(x_test)
+print(lg_prediction)
+
+
+mse = mean_squared_error(lg_prediction,y_test)
+
+rmse = math.sqrt(mse)
+
+print(rmse)
